cff4c00dec65a23400f5f086e83a5b8f777b2c7e
shore <372660931@qq.com>
2022-08-09 09:37:55 +0800

    update

--- sv/test.sv
+++ sv/test.sv
@@ -1,11 +1,36 @@
 module top (
-    output [9:0] Q
+    input wire d1,
+    input wire d2,
+    input wire cr1,
+    input wire cr2,
+    output wire tmp,
+    output wire tt
 );
-    bot bo(Q);
+
+    always @(*) begin
+        if (cr1)
+            tmp = wr1;
+        else if (cr2)
+            tmp = wr1;
+        else
+            tmp = wr2;
+    end
+
+    assign tt = wr1 & wr2;
+
 endmodule
 
-module bot(
-    inout [9:0] Q
-);
-    assign Q = 7;
-endmodule
+// module top (
+//     input a_in, b_in, c_in, d_in,
+//     input [1:0] sel,
+//     output d_out
+// );
+//     always @* begin
+//         case (sel)
+//             2'b00 : d_out = a_in;
+//             2'b01 : d_out = b_in;
+//             2'b10 : d_out = c_in;
+//             2'b11 : d_out = d_in;
+//         endcase
+//     end
+// endmodule
